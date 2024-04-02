37a6d875f6676080c6d21ccd6a1137fbb2bffef1
nayechan <66f@naver.com>
2024-04-03 00:21:00 +0900

    fix 1967

--- 1967/main.cpp
+++ 1967/main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 class Node;
 
-class Connection{
+class Connection {
 	shared_ptr<Node> from;
 	shared_ptr<Node> to;
 	int distance;
 
 public:
-	Connection(shared_ptr<Node> from, shared_ptr<Node> to, int distance) 
-        : from(from), to(to), distance(distance) {}
+	Connection(shared_ptr<Node> from, shared_ptr<Node> to, int distance)
+		: from(from), to(to), distance(distance) {}
 
-	shared_ptr<Node> GetFrom(){return from;}
-	shared_ptr<Node> GetTo(){return to;}
+	shared_ptr<Node> GetFrom() { return from; }
+	shared_ptr<Node> GetTo() { return to; }
 
 	int GetDistance()
 	{
@@ -24,48 +24,54 @@ public:
 	}
 };
 
-class Node{
+class Node {
 	int value;
 	vector<shared_ptr<Connection>> connections;
+	bool isLeaf;
 
 public:
-	Node(int value) : value(value){}
+	Node(int value) : value(value), isLeaf(true) {}
 
-    std::shared_ptr<Node> shared_from_this() {
-        return std::shared_ptr<Node>(this);
-    }
+	std::shared_ptr<Node> shared_from_this() {
+		return std::shared_ptr<Node>(this);
+	}
 
-	int GetValue(){return value;}
+	int GetValue() { return value; }
 
-	void Connect(shared_ptr<Node> node, int distance){
+	void Connect(shared_ptr<Node> node, int distance) {
 		ConnectSingleDirection(node, distance);
-		node->Connect(shared_from_this(), distance);
+		node->ConnectSingleDirection(shared_from_this(), distance);
 	}
 
-	void ConnectSingleDirection(shared_ptr<Node> node, int distance){
-		connections.push_back(make_shared<Connection>(shared_from_this(), &node, distance));
+	void ConnectSingleDirection(shared_ptr<Node> node, int distance) {
+		auto connection = make_shared<Connection>(shared_from_this(), node, distance);
+		connections.push_back(connection);
+
+		if (value < node->GetValue()) isLeaf = false;
 	}
 
-	int GetLongestLength(bool ** visited)
+	bool IsLeafNode() { return isLeaf; }
+
+	int GetLongestLength(bool** visited)
 	{
 		int visition = 0;
 		int maxDist = 0;
-		
-		*visited[value] = true;
 
-		for (auto && connection : connections)
+		(*visited)[value] = true;
+
+		for (auto&& connection : connections)
 		{
 			int to = connection->GetTo()->GetValue();
 
-			if(!*visited[to])
+			if (!(*visited)[to])
 			{
-				int dist = connection->GetTo()->GetLongestLength(visited);
-				if(maxDist < dist) maxDist = dist;
+				int dist = connection->GetTo()->GetLongestLength(visited) + connection->GetDistance();
+				if (maxDist < dist) maxDist = dist;
 			}
 
 		}
-		
-		*visited[value] = false;
+
+		(*visited)[value] = false;
 
 		return maxDist;
 	}
@@ -77,28 +83,29 @@ int main()
 
 	cin >> inputCount;
 
-	Node ** node = new Node*[inputCount+1];
-	
-	for(int i=0; i<=inputCount; ++i)
+	Node** node = new Node * [inputCount + 1];
+
+	for (int i = 0; i <= inputCount; ++i)
 	{
 		node[i] = new Node(i);
 	}
 
-	for(int i=1; i<=inputCount; ++i)
+	for (int i = 1; i <= inputCount-1; ++i)
 	{
 		int from, to, distance;
 
 		cin >> from >> to >> distance;
 
-		node[from]->Connect(make_shared<Node>(node[to]), distance);
+		node[from]->Connect(shared_ptr<Node>(node[to]), distance);
 	}
 
 	int max = -1;
-	for(int i=1; i<=inputCount; ++i)
+	for (int i = 1; i <= inputCount; ++i)
 	{
-		bool * visited = new bool[inputCount+1];
-	
-		for(int j=0; j<=inputCount; ++j)
+		if (!node[i]->IsLeafNode()) continue;
+		bool* visited = new bool[inputCount + 1];
+
+		for (int j = 0; j <= inputCount; ++j)
 		{
 			visited[j] = false;
 		}
